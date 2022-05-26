1a36db58b636730924a2bc7202b0d736da66f97d
Nosovitski <nosovitski.gleb@gmail.com>
2022-05-26 22:53:26 +0300

    fix

--- script.js
+++ script.js
@@ -12,6 +12,9 @@
         number_1 = array[1];
         number_2 = array[0];
 
+        console.log(number_1);
+        console.log(number_2);
+
         // Version 2 // Temporary number
 
         var number_1 = 5,
@@ -21,6 +24,8 @@
         buffer_number = number_2; // buffer_number = 10
         number_2 = number_1; // number_2 = 5
         number_1 = buffer_number; // number_1 = 10
+        console.log(number_1);
+        console.log(number_2);
 
         // Version 3 // Деструктурирующее присваивание
 
@@ -28,3 +33,5 @@
             number_2 = 10;
 
         [number_1, number_2] = [number_2, number_1] // number_2 = 5, number_1 = 10
+        console.log(number_1);
+        console.log(number_2);
